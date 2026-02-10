f8e8da00c0dbd17cd0b35a70659c5c12e69d5a83
daniel17522 <daniel312123312@gmail.com>
2026-02-10 19:00:27 +0600

    Volgin Daniel finished assignment

--- src/Library.java
+++ src/Library.java
@@ -10,26 +10,57 @@ public class Library {
 
     // TODO: Add book to array
     public void addBook(Book book) {
-        // implement
+        if (count >= books.length) {
+            System.out.println("Library is full");
+            return;
+        }
+        books[count++] = book;
     }
 
     // TODO: Display all books
     public void displayBooks() {
-        // implement
+        if (count <= 0) {
+            System.out.println("Library is empty");
+            return;
+        }
+        for (int i = 0; i < count; i++) {
+            System.out.println(books[i]);
+        }
     }
 
     // TODO: Search book by title
     public Book searchByTitle(String title) {
+        for (int i = 0; i < count; i++) {
+            if (books[i].getTitle().equalsIgnoreCase(title)) {
+                return books[i];
+            }
+        }
         return null;
     }
 
     // TODO: Borrow book by title
     public void borrowBook(String title) {
-        // implement
+        Book book = searchByTitle(title);
+        if (book == null) {
+            System.out.println("Dont have this book");
+        } else if (!book.isAvailable()) {
+            System.out.println("Book is busy");
+        } else {
+            book.borrowBook();
+            System.out.println("Take it");
+        }
     }
 
     // TODO: Return book by title
     public void returnBook(String title) {
-        // implement
+        Book book = searchByTitle(title);
+        if (book == null) {
+            System.out.println("Dont have this book");
+        } else if (book.isAvailable()) {
+            System.out.println("The book is already at library");
+        } else {
+            book.returnBook();
+            System.out.println("Returned");
+        }
     }
 }
